5372f4b9fff29676860bcbb32f0ad8d7bc0299cf
luiz tanure <letanure@gmail.com>
2020-02-03 21:14:22 +0100

    build: update prettier config, autofix lint

--- tests/e2e/globals.js
+++ tests/e2e/globals.js
@@ -41,7 +41,7 @@ module.exports = {
      *   return 'Firefox specific global';
      * }
      */
-  }
+  },
 
   /////////////////////////////////////////////////////////////////
   // Global hooks
@@ -100,4 +100,4 @@ module.exports = {
     cb();
   }
    */
-};
+}
