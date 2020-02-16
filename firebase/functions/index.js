0c213c2a02a28adb946599bb5a810b7380c7d982
luiz tanure <letanure@gmail.com>
2020-02-16 22:09:58 +0100

    feat: add user to firebase collection on create

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,44 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+if (!admin.apps.length) {
+  admin.initializeApp({})
+}
+
+function findEmailInProvider(providers) {
+  let email = null
+  providers.forEach((providerData) => {
+    if (providerData.email !== null && providerData.email !== '') {
+      email = providerData.email
+    }
+  })
+  return email
+}
+
+exports.registerNewUser = functions.auth.user().onCreate((user) => {
+  const email = user.email || findEmailInProvider(user.providerData)
+  const userData = {
+    email: email,
+    emailVerified: user.emailVerified,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    phoneNumber: user.phoneNumber,
+    disabled: user.disabled,
+    uid: user.uid,
+    providerData: user.providerData.map((providerData) => {
+      return {
+        providerId: providerData.providerId,
+        uid: providerData.uid,
+      }
+    }),
+  }
+  if (userData.uid) {
+    admin
+      .firestore()
+      .collection('users')
+      .add(userData)
+    return true
+  } else {
+    return false
+  }
+})
