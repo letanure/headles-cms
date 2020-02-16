165ce29ef2293ca7c1d56207cfdd4da275fd887b
luiz tanure <letanure@gmail.com>
2020-02-16 16:46:08 +0100

    refactor: move global styles out SFC

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,14 @@
 </template>
 
 <script>
+import '@/styles/index.styl'
+
 export default {
   name: 'App',
 }
 </script>
 
 <style lang="stylus">
-body
-  -moz-osx-font-smoothing grayscale
-  -webkit-font-smoothing antialiased
-  color #2c3e50
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  margin 0
-  padding 0
-  overflow-y scroll
 #app
   min-height 100vh
-
-  .el-form--label-top .el-form-item__label
-    float none
-    display inline-block
-    text-align left
-    padding 0 0 0px 0
-    line-height 35px
 </style>
