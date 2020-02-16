c963bc0f086f3086a43287280f84ea318e26e699
luiz tanure <letanure@gmail.com>
2020-02-16 22:12:43 +0100

    refactor: add scope to providers to getuser email

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -4,9 +4,38 @@ import 'firebase/auth'
 
 export const auth = firebase.auth()
 
-export const providers = {
-  facebook: new Firebase.auth.FacebookAuthProvider(),
-  github: new Firebase.auth.GithubAuthProvider(),
-  google: new Firebase.auth.GoogleAuthProvider(),
-  twitter: new Firebase.auth.TwitterAuthProvider(),
-}
+const providersConfig = [
+  {
+    name: 'facebook',
+    obj: new Firebase.auth.FacebookAuthProvider(),
+    scopes: ['email'],
+  },
+  {
+    name: 'github',
+    obj: new Firebase.auth.GithubAuthProvider(),
+    scopes: ['user:email'],
+  },
+  {
+    name: 'google',
+    obj: new Firebase.auth.GoogleAuthProvider(),
+    scopes: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  },
+  {
+    name: 'twitter',
+    obj: new Firebase.auth.TwitterAuthProvider(),
+    scopes: [],
+  },
+]
+
+const providersWithScope = {}
+providersConfig.forEach((provider) => {
+  provider.scopes.forEach((scope) => {
+    provider.obj.addScope(scope)
+  })
+  providersWithScope[provider.name] = provider.obj
+})
+
+export const providers = providersWithScope
