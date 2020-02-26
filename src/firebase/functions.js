431a6fb97a44e8aa2da2732d6e493c577c826ec3
luiz tanure <letanure@gmail.com>
2020-02-26 23:11:55 +0100

    refactor: remove deleted firebase callable functions

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -7,10 +7,6 @@ if (process.env.VUE_APP_FIREBASE === 'emulator') {
   firebaseApp.functions().useFunctionsEmulator('http://localhost:5001')
 }
 
-export const createUser = firebaseApp.functions().httpsCallable('createUser')
-
-export const deleteUser = firebaseApp.functions().httpsCallable('deleteUser')
-
 export const updateUser = firebaseApp.functions().httpsCallable('updateUser')
 
 export const getPageCollection = firebaseApp
