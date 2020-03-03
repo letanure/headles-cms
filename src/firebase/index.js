4e174c6f4fa65c1528aae8c475b3cb4023f8d750
luiz tanure <letanure@gmail.com>
2020-03-03 21:56:51 +0100

    feat: add error message in firebase collection wrapper

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,17 @@
+import i18n from '@/plugins/i18n'
 import { firestore, serverTimestamp } from '@/firebase/firestore'
 import { getPageCollection } from '@/firebase/functions'
+import { MessageBox } from 'element-ui'
+
+function errorMessage(errorCode){
+  MessageBox.alert(
+    i18n.tc(`general.messages.${errorCode}.message`),
+    i18n.tc(`general.messages.${errorCode}.title`),
+    {
+      type: 'error',
+    },
+  )
+}
 
 export default class FirestoreWrapper {
   constructor(options = {}) {
@@ -9,62 +21,89 @@ export default class FirestoreWrapper {
   }
 
   getDoc(id) {
-    return firestore.doc(`${this.collection}/${id}`)
+    try {
+      return firestore.doc(`${this.collection}/${id}`)
+    } catch (error) {
+      errorMessage(error.code)
+    }
   }
 
   getCollection() {
-    console.log('getCollection', this.collection)
-    return firestore.collection(this.collection)
+    try {
+      return firestore.collection(this.collection)
+    } catch (error) {
+      errorMessage(error.code)
+    }
   }
 
   async addItem(data, status = 'ACTIVE') {
-    return this.getCollection().add({
-      status: status,
-      created: serverTimestamp,
-      ...data,
-    })
+    try {
+      return this.getCollection().add({
+        status: status,
+        created: serverTimestamp,
+        ...data,
+      })
+    } catch (error) {
+      errorMessage(error.code)
+    }
   }
 
   async updateItem(id, data) {
-    const docSnap = this.getDoc(id)
-    return await docSnap.set(
-      {
-        updated: serverTimestamp,
-        ...data,
-      },
-      { merge: true },
-    )
+    try {
+      const docSnap = this.getDoc(id)
+      return await docSnap.set(
+        {
+          updated: serverTimestamp,
+          ...data,
+        },
+        { merge: true },
+      )
+    } catch (error) {
+      errorMessage(error.code)
+    }
   }
 
   async getItem(id) {
-    const docSnap = await this.getDoc(id).get()
-    const data = await docSnap.data()
-    return {
-      id: docSnap.id,
-      ref: docSnap.ref,
-      exists: docSnap.exists,
-      data: data,
+    try {
+      const docSnap = await this.getDoc(id).get()
+      const data = await docSnap.data()
+      return {
+        id: docSnap.id,
+        ref: docSnap.ref,
+        exists: docSnap.exists,
+        data: data,
+      }
+    } catch (error) {
+      errorMessage(error.code)
     }
   }
 
   async deleteItem(id, status = 'DELETED') {
-    const docSnap = this.getDoc(id)
-    return await docSnap.set(
-      {
-        deleted: serverTimestamp,
-        status: status,
-      },
-      { merge: true },
-    )
+    try {
+      const docSnap = this.getDoc(id)
+      return await docSnap.set(
+        {
+          deleted: serverTimestamp,
+          status: status,
+        },
+        { merge: true },
+      )
+    } catch (error) {
+      errorMessage(error.code)
+    }
   }
 
   async getPage({ page = 1 }) {
-    const req = await getPageCollection({
-      collection: this.collection,
-      orderBy: this.orderBy,
-      limit: this.perPage,
-      offset: (page - 1) * this.perPage,
-    })
-    return req.data
+    try {
+      const req = await getPageCollection({
+        collection: this.collection,
+        orderBy: this.orderBy,
+        limit: this.perPage,
+        offset: (page - 1) * this.perPage,
+      })
+      return req.data
+    } catch (error) {
+      errorMessage(error.code)
+    }
   }
 }
