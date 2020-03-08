82e5d9d876bce3919d68984cc6e2700a139abbf6
luiz tanure <letanure@gmail.com>
2020-03-08 16:08:39 +0100

    test: init test register form

---
+++ tests/unit/components/Auth/RegisterForm.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/Auth/RegisterForm.vue'
+
+describe(`${Component.name}.vue`, () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Component, {
+      propsData: {},
+      mocks: {
+        $t: (msg) => msg,
+      },
+    })
+  })
+
+  describe('smoke tests', () => {
+    it('should render', () => expect(wrapper.exists()).toBe(true))
+    it('matches snapshot', () => expect(wrapper.element).toMatchSnapshot())
+  })
+})
