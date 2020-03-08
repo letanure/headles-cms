0b48bc7434e8ee37e6f0620442740d623a6bc728
luiz tanure <letanure@gmail.com>
2020-03-08 16:57:24 +0100

    build: upgrade cue clie packages

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -5,20 +5,18 @@ Vue.use(VueI18n)
 
 function loadLocaleMessages() {
   const messages = {}
-  if (require?.context) {
-    const locales = require.context(
-      '../locales',
-      true,
-      /[A-Za-z0-9-_,\s]+\.json$/i,
-    )
-    locales.keys().forEach((key) => {
-      const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-      if (matched && matched.length > 1) {
-        const locale = matched[1]
-        messages[locale] = locales(key)
-      }
-    })
-  }
+  const locales = require.context(
+    '../locales',
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i,
+  )
+  locales.keys().forEach((key) => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1]
+      messages[locale] = locales(key)
+    }
+  })
   return messages
 }
 
