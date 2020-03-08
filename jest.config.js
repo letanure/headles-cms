a7c559ee57bf721831a88ec4ec3ed1a852db41a5
luiz tanure <letanure@gmail.com>
2020-03-08 17:07:46 +0100

    test: refactor require.context

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
+  setupFiles: ['<rootDir>/tests/unit/register-context.js'],
 }
---
+++ tests/unit/register-context.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line vue/max-len
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+registerRequireContextHook()
