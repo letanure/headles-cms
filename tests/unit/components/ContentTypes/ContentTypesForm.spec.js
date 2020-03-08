44f9edd4d8a3d14f5ee785d9788747e4739ffc48
luiz tanure <letanure@gmail.com>
2020-03-08 16:09:20 +0100

    test: init test content type

---
+++ tests/unit/components/ContentTypes/ContentTypesForm.spec.js
@@ -0,0 +1,21 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/ContentTypes/ContentTypesForm.vue'
+require('babel-plugin-require-context-hook/register')()
+
+describe(`${Component.name}.vue`, () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Component, {
+      propsData: {},
+      mocks: {
+        $t: (msg) => msg,
+      },
+    })
+  })
+
+  describe('smoke tests', () => {
+    it('should render', () => expect(wrapper.exists()).toBe(true))
+    it('matches snapshot', () => expect(wrapper.element).toMatchSnapshot())
+  })
+})
