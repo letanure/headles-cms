e4a5a2406acac07a6383b69faa5ad065b3dd5c52
luiz tanure <letanure@gmail.com>
2020-03-09 23:31:05 +0100

    refactor: list of fields based on types exported

--- src/components/ContentTypes/SelectFieldType.vue
+++ src/components/ContentTypes/SelectFieldType.vue
@@ -3,12 +3,13 @@
     el-option(
       v-for="option in fieldOptions"
       :key="option.value"
-      :label="option.label"
+      :label="$t(`fieldType.${option.value}`)"
       :value="option.value"
     )
 </template>
 
 <script>
+import InputTypes from '@/components/ContentTypes/FieldTypes.ts'
 import { Option, Select } from 'element-ui'
 
 export default {
@@ -27,70 +28,12 @@ export default {
     },
   },
 
-  data() {
-    return {
-      fieldOptions: [
-        {
-          label: this.$t('fieldTyle.checkbox.label'),
-          value: 'checkbox',
-        },
-        {
-          label: this.$t('fieldTyle.color.label'),
-          value: 'color',
-        },
-        {
-          label: this.$t('fieldTyle.date.label'),
-          value: 'date',
-        },
-        {
-          label: this.$t('fieldTyle.email.label'),
-          value: 'email',
-        },
-        {
-          label: this.$t('fieldTyle.month.label'),
-          value: 'month',
-        },
-        {
-          label: this.$t('fieldTyle.number.label'),
-          value: 'number',
-        },
-        {
-          label: this.$t('fieldTyle.password.label'),
-          value: 'password',
-        },
-        {
-          label: this.$t('fieldTyle.radio.label'),
-          value: 'radio',
-        },
-        {
-          label: this.$t('fieldTyle.range.label'),
-          value: 'range',
-        },
-        {
-          label: this.$t('fieldTyle.tel.label'),
-          value: 'tel',
-        },
-        {
-          label: this.$t('fieldTyle.text.label'),
-          value: 'text',
-        },
-        {
-          label: this.$t('fieldTyle.time.label'),
-          value: 'time',
-        },
-        {
-          label: this.$t('fieldTyle.url.label'),
-          value: 'url',
-        },
-        {
-          label: this.$t('fieldTyle.week.label'),
-          value: 'week',
-        },
-      ],
-    }
-  },
-
   computed: {
+    fieldOptions() {
+      return Object.values(InputTypes).map((InputType) => ({
+        value: InputType.type,
+      }))
+    },
     localValue: {
       get() {
         return this.value
